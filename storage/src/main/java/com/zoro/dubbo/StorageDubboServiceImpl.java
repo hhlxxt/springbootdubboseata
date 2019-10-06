6c66ab5d1e7c54b0ee5b246b8dd7afaa23f77969
hhlxxt <hhlx881101>
2019-10-06 22:26:14 +0800

    修改使用slf4j打印日志

--- storage/src/main/java/com/zoro/dubbo/StorageDubboServiceImpl.java
+++ storage/src/main/java/com/zoro/dubbo/StorageDubboServiceImpl.java
@@ -5,6 +5,7 @@ import com.zoro.dto.CommodityDTO;
 import com.zoro.response.ObjectResponse;
 import com.zoro.service.ITStorageService;
 import io.seata.core.context.RootContext;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
@@ -13,13 +14,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 @Service(version = "1.0.0",protocol = "${dubbo.protocol.id}",
         application = "${dubbo.application.id}",registry = "${dubbo.registry.id}",
         timeout = 3000)
+@Slf4j
 public class StorageDubboServiceImpl implements StorageDubboService {
 
     @Autowired
     private ITStorageService storageService;
 
     public ObjectResponse decreaseStorage(CommodityDTO commodityDTO) {
-        System.out.println("全局事务id ：" + RootContext.getXID());
+        log.info("库存服务全局事务id ：{}" , RootContext.getXID());
         return storageService.decreaseStorage(commodityDTO);
     }
 }
