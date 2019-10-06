e4a56f913f08b45372f5d916eae3132b712e1277
hhlxxt <hhlx881101>
2019-10-06 22:33:18 +0800

    修改使用slf4j打印日志

--- account/src/main/java/com/zoro/dubbo/AccountDubboServiceImpl.java
+++ account/src/main/java/com/zoro/dubbo/AccountDubboServiceImpl.java
@@ -5,6 +5,7 @@ import com.zoro.dto.AccountDTO;
 import com.zoro.response.ObjectResponse;
 import com.zoro.service.ITAccountService;
 import io.seata.core.context.RootContext;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
@@ -13,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 @Service(version = "1.0.0",protocol = "${dubbo.protocol.id}",
          application = "${dubbo.application.id}",registry = "${dubbo.registry.id}",
          timeout = 3000)
+@Slf4j
 public class AccountDubboServiceImpl implements AccountDubboService {
 
     @Autowired
@@ -20,7 +22,7 @@ public class AccountDubboServiceImpl implements AccountDubboService {
 
     @Override
     public ObjectResponse decreaseAccount(AccountDTO accountDTO) {
-        System.out.println("全局事务id ：" + RootContext.getXID());
+        log.info("账户服务全局事务id:{}",RootContext.getXID());
         return accountService.decreaseAccount(accountDTO);
     }
 }
