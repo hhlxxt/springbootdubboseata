6004739ed52092ee6930cc3ab9d96223a5077226
hhlxxt <hhlx881101>
2019-10-06 21:59:09 +0800

    修改注释

--- db/db.sql
+++ db/db.sql
@@ -4,13 +4,12 @@ Navicat MySQL Data Transfer
 Source Server         : account
 Source Server Version : 50614
 Source Host           : localhost:3306
-Source Database       : db_gts_fescar
+Source Database       : dubboseata
 
 Target Server Type    : MYSQL
 Target Server Version : 50614
 File Encoding         : 65001
 
-Date: 2019-01-26 10:23:10
 */
 
 SET FOREIGN_KEY_CHECKS=0;
